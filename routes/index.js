bd19026abc0600d289c54b873598d010790142ea
Marianna <mddobal@gmail.com>
2019-02-01 15:58:10 -0200

    done

--- routes/index.js
+++ routes/index.js
@@ -28,4 +28,53 @@ router.get('/book/:id', (req, res, next) => {
     })
 });
 
+router.get('/books/add', (req, res, next) => {
+  res.render("book-add");
+});
+
+router.post('/books/add', (req, res, next) => {
+  const { title, author, description, rating } = req.body; // desconstrução de obj
+  const newBook = new BookModel({ title, author, description, rating});
+  newBook.save()
+    .then((book) => {
+      res.redirect('/books');
+    })
+    .catch((error) => {
+      console.log(error);
+  })
+});
+
+router.get('/books/edit/:id', (req, res, next) => {
+  BookModel.findOne({ "_id": req.params.id })
+    .then((book) => {
+      res.render("book-edit", { book });
+    })
+    .catch(err => {
+      console.log(err);
+    })
+});
+
+router.post('/books/edit', (req, res, next) => {
+  const { title, author, description, rating } = req.body;
+  BookModel.update({_id: req.query.bookId}, { $set: {title, author, description, rating }})
+  .then((book) => {
+    res.redirect('/books');
+  })
+  .catch((error) => {
+    console.log(error);
+  })
+});
+
+router.get('/books/del/:id', (req, res, next) => {
+  BookModel.deleteOne({ "_id": req.params.id })
+    .then(() => {
+      res.redirect('/books');
+    })
+    .catch(err => {
+      console.log(err);
+    })
+});
+
+
+
 module.exports = router;
